c4d377e27e3d52411d2984eb5c438fb36f6c5d3e
koolade44 <derstein04@gmail.com>
2022-09-18 22:49:54 -0600

    added sleep so relays can be spaced close together and not bounce off each other

--- src/firmware/relay-firmware-1.0.0.lua
+++ src/firmware/relay-firmware-1.0.0.lua
@@ -1,8 +1,14 @@
 -- Firmware for microcontroller relays
 
 local m = component.proxy(component.list("modem")())
+local clock = os.clock()
 m.setStrength(400)
 m.open(443)
+
+function sleep(n)
+    local t0 = clock()
+    while clock - t0 < n do end
+end
 
 while true do
     local name, _, _, port, _, message = computer.pullSignal()
@@ -10,5 +16,6 @@ while true do
     if (name == "modem_message") then
         computer.beep(1000, 0.3)
         m.broadcast(port, message)
+        sleep(2)
     end
 end
