55f4dba904c6498530a8c5c7449a5f62e6d9d7aa
koolade44 <derstein04@gmail.com>
2022-09-18 21:08:04 -0600

    microcontroller firmware version 1.0.0

---
+++ src/firmware/relay-firmware-1.0.0.lua
@@ -0,0 +1,19 @@
+-- Firmware for microcontroller relays
+
+local m = component.proxy(component.list("modem")())
+
+while true do
+    local name, data = computer.pullSignal()
+    if (name == "modem_message") then
+        local _, _, from, port, _, message = data
+        relaySignal(from, port, message)
+    end
+end
+
+function relaySignal(from, port, message)
+    computer.beep(1000, 0.3)
+    if not m.isOpen(port) then
+        m.open(port)
+    end
+    m.broadcast(port, message)
+end
