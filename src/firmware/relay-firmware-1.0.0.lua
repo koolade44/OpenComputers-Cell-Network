aac8fb8774dd89084ffc5dddda28cb0702e7597e
koolade44 <derstein04@gmail.com>
2022-09-18 21:17:22 -0600

    microcontroller firmware version 1.0.0

--- src/firmware/relay-firmware-1.0.0.lua
+++ src/firmware/relay-firmware-1.0.0.lua
@@ -5,12 +5,13 @@ local m = component.proxy(component.list("modem")())
 while true do
     local name, data = computer.pullSignal()
     if (name == "modem_message") then
-        local _, _, from, port, _, message = data
-        relaySignal(from, port, message)
+        local port = data[3]
+        local message = data[5]
+        relaySignal(port, message)
     end
 end
 
-function relaySignal(from, port, message)
+function relaySignal(port, message)
     computer.beep(1000, 0.3)
     if not m.isOpen(port) then
         m.open(port)
